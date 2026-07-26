cb17d278a0bc706587cce3a49863e016a7d23f3d
ishqafathima <149235722+ishqafathima@users.noreply.github.com>
2026-07-27 01:18:16 +0530

    Add files via upload

---
+++ .expo/Types/router.d.ts
@@ -0,0 +1,14 @@
+/* eslint-disable */
+import * as Router from 'expo-router';
+
+export * from 'expo-router';
+
+declare module 'expo-router' {
+  export namespace ExpoRouter {
+    export interface __routes<T extends string | object = string> {
+      hrefInputParams: { pathname: Router.RelativePathString, params?: Router.UnknownInputParams } | { pathname: Router.ExternalPathString, params?: Router.UnknownInputParams } | { pathname: `/`; params?: Router.UnknownInputParams; } | { pathname: `/_sitemap`; params?: Router.UnknownInputParams; } | { pathname: `/login/Login`; params?: Router.UnknownInputParams; } | { pathname: `/login/Register`; params?: Router.UnknownInputParams; } | { pathname: `/movie/favorites`; params?: Router.UnknownInputParams; } | { pathname: `/movie`; params?: Router.UnknownInputParams; } | { pathname: `/movie/profile`; params?: Router.UnknownInputParams; } | { pathname: `/movie/movieid/[id]`, params: Router.UnknownInputParams & { id: string | number; } };
+      hrefOutputParams: { pathname: Router.RelativePathString, params?: Router.UnknownOutputParams } | { pathname: Router.ExternalPathString, params?: Router.UnknownOutputParams } | { pathname: `/`; params?: Router.UnknownOutputParams; } | { pathname: `/_sitemap`; params?: Router.UnknownOutputParams; } | { pathname: `/login/Login`; params?: Router.UnknownOutputParams; } | { pathname: `/login/Register`; params?: Router.UnknownOutputParams; } | { pathname: `/movie/favorites`; params?: Router.UnknownOutputParams; } | { pathname: `/movie`; params?: Router.UnknownOutputParams; } | { pathname: `/movie/profile`; params?: Router.UnknownOutputParams; } | { pathname: `/movie/movieid/[id]`, params: Router.UnknownOutputParams & { id: string; } };
+      href: Router.RelativePathString | Router.ExternalPathString | `/${`?${string}` | `#${string}` | ''}` | `/_sitemap${`?${string}` | `#${string}` | ''}` | `/login/Login${`?${string}` | `#${string}` | ''}` | `/login/Register${`?${string}` | `#${string}` | ''}` | `/movie/favorites${`?${string}` | `#${string}` | ''}` | `/movie${`?${string}` | `#${string}` | ''}` | `/movie/profile${`?${string}` | `#${string}` | ''}` | { pathname: Router.RelativePathString, params?: Router.UnknownInputParams } | { pathname: Router.ExternalPathString, params?: Router.UnknownInputParams } | { pathname: `/`; params?: Router.UnknownInputParams; } | { pathname: `/_sitemap`; params?: Router.UnknownInputParams; } | { pathname: `/login/Login`; params?: Router.UnknownInputParams; } | { pathname: `/login/Register`; params?: Router.UnknownInputParams; } | { pathname: `/movie/favorites`; params?: Router.UnknownInputParams; } | { pathname: `/movie`; params?: Router.UnknownInputParams; } | { pathname: `/movie/profile`; params?: Router.UnknownInputParams; } | `/movie/movieid/${Router.SingleRoutePart<T>}${`?${string}` | `#${string}` | ''}` | { pathname: `/movie/movieid/[id]`, params: Router.UnknownInputParams & { id: string | number; } };
+    }
+  }
+}
